9a936d6d44e465ae464ddc5d02978874c685d56b
Ben Lowry <benlowry@outlook.com>
2019-07-10 14:39:05 -0600

    Cleaned formatting

--- hastebin/hastebin-app-store-subscription/start-dev.sh
+++ hastebin/hastebin-app-store-subscription/start-dev.sh
@@ -1,27 +1,5 @@
-# ------------------------
-# Authorize a specific app store:
-# (via @userdashboard/express-application-server)
-# ---------------------------------
-# AUTHORIZE_APP_STORE_1=https://..... \
-# AUTHORIZE_APP_STORE_1_TOKEN=.... \
-# APPLICATION_SERVER_1_TOKEN=.... \
-#
-# ----------------------------------------------------
-# Ban an app store
-# (via @userdashboard/express-application-server)
-# --------------------------------------------------
-# PROHIBIT_APP_STORE_1=https://....
-# PROHIBIT_APP_STORE_2=https://....
-#
-# ----------------------------------------------------
-# Enable users to import on any app store 
-# (via server.js)
-# --------------------------------------------------
-# ANONYMOUS_APP_STORES=true
-#
-
 AUTHORIZE_APP_STORE_1=https://userappstore.server7373.synology.me \
-AUTHORIZE_APP_STORE_1_TOKEN=\$2a\$04\$TTiJ6MHrYXr/q0DCJTedeupXWQACaZTcHL2DKSE.i1yM.4jcAqbxi \
+AUTHORIZE_APP_STORE_1_TOKEN=\$2a\$04\$G2ndMhxCcuZGG1lASvjG3.1gFWaLpNCQH3IqzZr4644prDc7pv4w6 \
 APPLICATION_SERVER_1_TOKEN=f57TCTpveTIiDuXgsT5oB0ZDRgSKojW86fzTKpvvOmbCotawQXILLdB5qpDcqqvb \
 PUBLIC_DOMAIN="test-application.server7373.synology.me" \
 PORT=8300 \
